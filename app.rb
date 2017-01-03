8362afc691946029633a28a177065e06940eedb1
Carly Ho <cho@cliquestudios.com>
2017-01-03 15:52:50 -0600

    Add more files, update rakefile and add database stuff

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require 'sinatra'
 require 'sinatra-activerecord'
 
+set :database_file, 'config/database.yml'
+
+class User < ActiveRecord::Base
+end
+
+class Post < ActiveRecord::Base
+end
+
+get '/' do
+	
+end
